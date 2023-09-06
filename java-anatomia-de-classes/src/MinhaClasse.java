58f6d377adc1952437683596af3d48a1bd5abe34
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-06 19:17:36 -0300

    Praticando nomenclatura

---
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+  public static void main(String[] args) {
+    String $pais = "Nova Zelândia";
+    int numeroum = 1;
+    int numero2 = 2;
+    String _capital = "Brasília";
+    Strimg palavraComposta = "Cachorro-quente";
+  }
+}
