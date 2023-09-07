9ba3208310c2961a4775e1fc5e73527ace3d2811
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-07 19:31:51 -0300

    Adcionando metodos para manipular canal

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -3,12 +3,26 @@ public class SmartTv {
   int canal = 1;
   int volume = 25;
 
+  public void mudarCanal(int novoCanal) {
+    canal = novoCanal;
+  }
+
+  public void aumentarCanal() {
+    canal++;
+  }
+
+  public void diminuirCanal() {
+    canal--;
+  }
+
   public void aumentarVolume() {
     volume++;
+    System.out.println("Aumentando volume para: " + volume);
   }
 
   public void diminuirVolume() {
     volume--;
+    System.out.println("Dimnuindo volume para: " + volume);
   }
 
   public void ligar() {
