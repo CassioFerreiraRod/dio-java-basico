4616f598d704bfd94a224288489f939c693d2436
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-07 18:38:08 -0300

    Atualiazando a classe Incremento

--- tipos-variaveis/src/operadores/Incremento.java
+++ tipos-variaveis/src/operadores/Incremento.java
@@ -1,13 +1,12 @@
 package operadores;
-
 public class Incremento {
   public static void main(String[] args) {
-    int numero = 5;
+    int numero = 6;
 
-    System.out.println(numero++);
+    System.out.println(numero ++);
 
-    System.out.println(++numero);
+    System.out.println(++ numero);
 
-    System.out.println(--numero);
+    System.out.println(-- numero);
   }
 }
