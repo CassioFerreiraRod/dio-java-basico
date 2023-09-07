7abccc05dac6e8dfc314c5779024424560805b61
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-07 17:46:52 -0300

    Commitando as classe Operadores e Incremento

---
+++ tipos-variaveis/src/Incremento.java
@@ -0,0 +1,11 @@
+public class Incremento {
+  public static void main(String[] args) {
+    int numero = 6;
+
+    System.out.println(numero ++);
+
+    System.out.println(++ numero);
+
+    System.out.println(-- numero);
+  }
+}
