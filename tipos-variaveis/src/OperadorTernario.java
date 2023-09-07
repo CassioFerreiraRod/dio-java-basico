d52584a61cd8e5740188daca2f3af46368c205c1
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-07 17:54:47 -0300

    Commitando a classe OperaodorTernario

---
+++ tipos-variaveis/src/OperadorTernario.java
@@ -0,0 +1,19 @@
+public class OperadorTernario {
+  public static void main(String[] args) {
+    int a,b;
+    a = 5;
+    b = 6;
+
+    String resultado = a == b ? "Verdadeiro" : "Falso";
+
+    System.out.println(resultado);
+
+    int c,d;
+    c = 2;
+    d = 5; 
+
+    int resultado1 = c == d ? 1 : 0;
+
+    System.out.println(resultado1);
+  }
+}
