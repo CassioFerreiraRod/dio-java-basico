2b252b3fddd3a004054f48a7485bd5a477a4d0e6
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-07 19:24:15 -0300

    Adicionando os metodos de ligar e desligar

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -5,5 +5,13 @@ public class Usuario {
         System.out.println("TV ligada? " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
     }
 }
