634fe796f182ab60f3695c194fd2818ba9a1c734
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-08 17:42:17 -0300

    nova classe atualizando CaixaEletronico

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,12 +1,13 @@
 public class CaixaEletronico {
   public static void main(String[] args) {
     double saldo = 25.0;
-    double valorSolicidado = 27.0;
+    double valorSolicidado = 22.0;
 
     if (valorSolicidado < saldo) {
       saldo -= valorSolicidado;
-    }
+      System.out.println("O novo saldo é:" + saldo);
+    } else
+      System.out.println("Saldo insulficiente.");
 
-    System.out.println(saldo);
   }
 }
